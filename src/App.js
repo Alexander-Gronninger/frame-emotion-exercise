2b93b3c750bea09a09b0cba2626cd2a904e5a00a
Alexander-Gronninger <alexander.gronninger@gmail.com>
2022-08-19 08:35:56 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
+import { motion } from "framer-motion";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const container = {
+    hidden: {
+      opacity: 0,
+    },
+    show: {
+      opacity: 1,
+      transition: {
+        delayChildren: 0.5,
+      },
+    },
+  };
+
+  const item = {
+    hidden: {
+      opacity: 0,
+      scale: 0.5,
+    },
+    show: {
+      opacity: 1,
+      scale: 1,
+    },
+  };
+
+  return (
+    <div className="App">
+      <motion.div
+        variants={container}
+        initial="hidden"
+        animate="show"
+        transition={{
+          ease: "easeInOut",
+          duration: 0.5,
+          type: "spring",
+        }}
+      >
+        <motion.h1 variants={item}>Bummelum!</motion.h1>
+        <h2>lololololol</h2>
+      </motion.div>
+    </div>
+  );
 }
 
 export default App;
